2f6a208855fc51a2af586236e53179d8788d8a78
Prakhar Trivedi <53103894+Prakhar896@users.noreply.github.com>
2023-03-30 23:20:16 +0800

    Initial Commit

---
+++ BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by Prakhar Trivedi on 30/3/23.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
