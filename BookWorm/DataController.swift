0932914394b586da732dcc80e5a6cf3b896dd279
Prakhar Trivedi <53103894+Prakhar896@users.noreply.github.com>
2023-03-31 22:58:48 +0800

    completed creating books with Core Data section

--- BookWorm/DataController.swift
+++ BookWorm/DataController.swift
@@ -9,10 +9,7 @@ import CoreData
 import Foundation
 
 class DataController: ObservableObject {
-    let container = NSPersistentContainer(name: "Bookworm") // we are telling core data to use our Bookworm data model for its container, the data hasn't been loaded yet
-    
-    // Important Note: Data models are NOT the same as data. Data models just define the types, properties and relationships a piece of data can have.
-    // To actually load things, we have to go ahead and say please try and load your data and Core Data will pull out the data from permanent disk storage.
+    let container = NSPersistentContainer(name: "Bookworm")
     
     init() {
         container.loadPersistentStores { description, error in
