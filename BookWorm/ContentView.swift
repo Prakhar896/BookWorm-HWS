931992ca39f4cd4e203d5873f1f2a34645c5805c
Prakhar Trivedi <Prakhar896@users.noreply.github.com>
2023-08-23 23:00:12 +0800

    added deletion capabilities

--- BookWorm/ContentView.swift
+++ BookWorm/ContentView.swift
@@ -37,6 +37,7 @@ struct ContentView: View {
                         }
                     }
                 }
+                .onDelete(perform: deleteBooks)
             }
             .navigationTitle("Bookworm")
             .toolbar {
@@ -47,11 +48,24 @@ struct ContentView: View {
                         Label("Add Book", systemImage: "plus")
                     }
                 }
+                
+                ToolbarItem(placement: .navigationBarLeading) {
+                    EditButton()
+                }
             }
             .sheet(isPresented: $showingAddScreen) {
                 AddBookView()
             }
         }
+    }
+    
+    func deleteBooks(at offsets: IndexSet) {
+        for offset in offsets {
+            let book = books[offset]
+            moc.delete(book)
+        }
+        
+        try? moc.save()
     }
 }
 
